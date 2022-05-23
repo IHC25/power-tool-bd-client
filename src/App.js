6062e8b15ae687e67ee6ff6057ad36d3a038b2c3
arefin6 <imranhchy@gmail.com>
2022-05-23 11:33:33 +0600

    simple footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Pages/Shared/Footer";
 import Header from "./Pages/Shared/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
