4c9b5ca14df358a1591c32d0e5b388cd633c7a91
arefin6 <imranhchy@gmail.com>
2022-05-26 14:19:57 +0600

    firebase init secured

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_OgDmCsnAVnE_bjLA6wrPCjKrHLgxJPU",
-  authDomain: "power-tool-bd.firebaseapp.com",
-  projectId: "power-tool-bd",
-  storageBucket: "power-tool-bd.appspot.com",
-  messagingSenderId: "93443525680",
-  appId: "1:93443525680:web:e44bb500e399edd260f1f5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
